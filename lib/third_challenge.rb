1d4a03e9de7cbb73f9a6788f0f82c26f286a6711
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-02 02:54:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -19,14 +19,14 @@ def third_challenge
    :capulet => {
       :patriarch => {
         name:"Lord Capulet",
-        age:  => "50"
+        age: "50"
       :matriarch => {
-        name: => "Lady Capulet",
+        name: "Lady Capulet",
         age:  "51"
       :heroine => {
-       name: => "Juliet",
+       name: "Juliet",
         age: "15",
-        :status => "alive"
+        status: "alive"
       :heroine_friends => []
    }
   }
